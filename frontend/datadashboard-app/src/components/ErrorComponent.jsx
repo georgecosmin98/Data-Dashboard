0648d5812858a4d1d7ceac05f1c1aedd830ab45c
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-11-16 19:49:52 +0200

    Styling error page

--- frontend/datadashboard-app/src/components/ErrorComponent.jsx
+++ frontend/datadashboard-app/src/components/ErrorComponent.jsx
@@ -1,14 +1,17 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-class ErrorComponent extends Component{
+class ErrorComponent extends Component {
 
-render(){
-    return(
-        <div>
-            <h1>Error 404</h1> 
-            <h1>This is an incorect URI!</h1>
-        </div>
-    )
-}
+    render() {
+        return (
+            <div className="ErrorComponent">
+                <div className="NotFound">
+                    <h2>Page not found</h2>
+                    <h1>404</h1>
+                    <h3>Sorry, we couldn't find the page you were looking for</h3>
+                </div>
+            </div>
+        )
+    }
 }
 export default ErrorComponent
