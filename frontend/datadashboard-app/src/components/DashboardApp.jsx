c5d313174a58e576c508eba8bf2d614f554e6a70
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-11-16 11:14:32 +0200

    Creating a simple error page for bad URI

--- frontend/datadashboard-app/src/components/DashboardApp.jsx
+++ frontend/datadashboard-app/src/components/DashboardApp.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import DashboardComponent from './DashboardComponent'
+import ErrorComponent from './ErrorComponent'
 
 class DashboardApp extends Component {
 
@@ -8,9 +9,12 @@ class DashboardApp extends Component {
         return (
             <div className="DashboardApp">
                 <Router>
+                    <>
                     <Switch>
                         <Route path="/" exact component={DashboardComponent}/>
+                        <Route component={ErrorComponent}/>
                     </Switch>
+                </>
                 </Router>
             </div>
         )
---
+++ frontend/datadashboard-app/src/components/ErrorComponent.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ErrorComponent extends Component{
+
+render(){
+    return(
+        <div>
+            <p>Error. This is an incorect URI!</p>
+        </div>
+    )
+}
+}
+export default ErrorComponent
