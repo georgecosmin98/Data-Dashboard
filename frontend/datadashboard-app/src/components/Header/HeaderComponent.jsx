a1eb1537218b368e84cf33ac98dfe928f9100b2b
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-14 10:24:58 +0200

    Make Active Sidebar Menu Button

--- frontend/datadashboard-app/src/components/Header/HeaderComponent.jsx
+++ frontend/datadashboard-app/src/components/Header/HeaderComponent.jsx
@@ -16,7 +16,6 @@ class HeaderComponent extends Component {
                 </div>
 
                 <div className="header-right">
-                    <FeedbackIcon className="header-icon" />
                     <Avatar
                         alt="Ilinca George-Cosmin"
                         src="https://avatars2.githubusercontent.com/u/37107231?s=400&u=58e114524f52c7e73ce81e850339664101e7384a&v=4"
--- frontend/datadashboard-app/src/components/SidebarComponent.jsx
+++ frontend/datadashboard-app/src/components/SidebarComponent.jsx
@@ -3,14 +3,14 @@ import SidebarRowComponent from './SidebarRowComponent'
 import HomeIcon from '@material-ui/icons/Home';
 import ErrorIcon from '@material-ui/icons/Error';
 import ContactMailIcon from '@material-ui/icons/ContactMail';
-
+import {NavLink} from 'react-router-dom'
 class SidebarComponent extends Component {
     render() {
         return (
             <div className="sidebar">
-                <a href="/"><SidebarRowComponent selected icon={HomeIcon} title="Home"></SidebarRowComponent> </a>
-                <a href="/error"> <SidebarRowComponent icon={ErrorIcon} title="ErrorPage"></SidebarRowComponent></a>
-                <a href="/contact"><SidebarRowComponent icon={ContactMailIcon} title="Contact" /></a>
+                <NavLink to="/" exact activeClassName="active"><SidebarRowComponent icon={HomeIcon} title="Home"></SidebarRowComponent> </NavLink>
+                <NavLink to="/error" activeClassName="active"><SidebarRowComponent icon={ErrorIcon} title="ErrorPage"></SidebarRowComponent></NavLink>
+                <NavLink to="/contact" activeClassName="active"><SidebarRowComponent icon={ContactMailIcon} title="Contact" /></NavLink>
             </div>
         )
     }
