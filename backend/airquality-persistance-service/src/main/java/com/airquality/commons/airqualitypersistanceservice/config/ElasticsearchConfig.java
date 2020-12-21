ed5decc973fd8f77d8a3510986dab2c6be2216a5
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-21 11:58:20 +0200

    Make elasticsearch configuration class

---
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/config/ElasticsearchConfig.java
@@ -0,0 +1,36 @@
+package com.airquality.commons.airqualitypersistanceservice.config;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    private String host;
+    private int port;
+    private String userName;
+    private String password;
+
+    @Bean(destroyMethod = "close")
+    public RestHighLevelClient restClient() {
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(userName, password));
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost(host, port, "https"))
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+
+        return client;
+    }
+}
