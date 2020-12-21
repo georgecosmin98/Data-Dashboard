bead1dd57af3611a40b663b0b82fb11119aea2f8
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-21 12:19:25 +0200

    Add properties for connection to elasticsearch

--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/config/ElasticsearchConfig.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/config/ElasticsearchConfig.java
@@ -8,15 +8,20 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticsearchConfig {
 
+    @Value("${elasticsearch.host}")
     private String host;
+    @Value("${elasticsearch.port}")
     private int port;
+    @Value("${elasticsearch.username}")
     private String userName;
+    @Value("${elasticsearch.password}")
     private String password;
 
     @Bean(destroyMethod = "close")
