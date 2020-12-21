8cd6877b79fe158c512c0fd29f620550664f1b32
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-21 11:24:51 +0200

    Initialize Sprin Boot project

---
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/AirqualityPersistanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.airquality.commons.airqualitypersistanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirqualityPersistanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirqualityPersistanceServiceApplication.class, args);
+	}
+
+}
---
+++ backend/airquality-persistance-service/src/test/java/com/airquality/commons/airqualitypersistanceservice/AirqualityPersistanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airquality.commons.airqualitypersistanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirqualityPersistanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
