fcd756f31beb0a8a0ac1b268dad7ba148bec117b
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-25 13:16:45 +0200

    Make EmailService implementation

---
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/api/EmailServiceImpl.java
@@ -0,0 +1,27 @@
+package com.airquality.commons.airqualitypersistanceservice.service.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService{
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Override
+    public void sendMail(String to, String subject, String text) {
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        //It's not mandatory to provide the from address, but many SMTP
+        //server would reject such messages
+        simpleMailMessage.setFrom("noreply@hartapoluarebrasov.com");
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(text);
+
+        javaMailSender.send(simpleMailMessage);
+    }
+}
