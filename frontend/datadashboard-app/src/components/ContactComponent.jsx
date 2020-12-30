d1d34707a57d0c9f09527fa1df6d49cf0b2b3405
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-30 11:10:57 +0200

    Migrate from html form to Formik form for contact page

--- frontend/datadashboard-app/src/components/ContactComponent.jsx
+++ frontend/datadashboard-app/src/components/ContactComponent.jsx
@@ -1,33 +1,85 @@
 import React, { Component } from 'react';
-
-
+import ContactService from './api/contact/ContactService'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 class ContactComponent extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+    }
+    
     render() {
+        let { name, email, message } = this.state
         return (
-            <div className="contact">
-                <div className="form">
-                    <h1 class="l-heading"><span class="text-primary">Contact </span>US</h1>
-                    <p>Your questions, comments and feedbacks are important for us!</p>
-                    <form action="process.php">
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" name="name" id="name" />
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="text" name="email" id="email" />
-                        </div>
-                        <div class="form-group">
-                            <label for="Message">Message</label>
-                            <textarea name="Message" id="Message"></textarea>
-                        </div>
-                        <div className="btn-center">
-                            <button type="submit" className="btn ">Submit</button>
-                        </div>
-                    </form>
+            // <div className="contact">
+            //     <div className="form">
+            //         <h1 class="l-heading"><span class="text-primary">Contact </span>US</h1>
+            //         <p>Your questions, comments and feedbacks are important for us!</p>
+            //         <form action="process.php">
+            //             <div class="form-group">
+            //                 <label for="name">Name</label>
+            //                 <input type="text" name="name" id="name" />
+            //             </div>
+            //             <div class="form-group">
+            //                 <label for="email">Email</label>
+            //                 <input type="text" name="email" id="email" />
+            //             </div>
+            //             <div class="form-group">
+            //                 <label for="Message">Message</label>
+            //                 <textarea name="message" id="Message"></textarea>
+            //             </div>
+            //             <div className="btn-center">
+            //                 <button type="submit" className="btn " onSubmit={this.onSubmit}>Submit</button>
+            //             </div>
+            //         </form>
+            //     </div>
+            // </div>
+                <div className="contact">
+                    <div className="form">
+                        <h1 class="l-heading"><span class="text-primary">Contact </span>US</h1>
+                        <p>Your questions, comments and feedbacks are important for us!</p>
+                        <Formik
+                            initialValues={{ name, email,message }}
+                            onSubmit={this.onSubmit}
+                            validateOnChange={false}
+                            validateOnBlur={false}
+                            validate={this.validate}
+                            enableReinitialize={true}
+                        >
+                            {
+                                (props) => (
+                                    <Form>
+                                        <ErrorMessage name="description" component="div"
+                                            className="alert alert-warning" />
+                                        <ErrorMessage name="targetDate" component="div"
+                                            className="alert alert-warning" />
+                                        <fieldset className="form-group">
+                                            <label>Name</label>
+                                            <Field className="input" type="text" name="name" />
+                                        </fieldset>
+                                        <fieldset className="form-group">
+                                            <label>Email</label>
+                                            <Field className="input" type="text" name="email" />
+                                        </fieldset>
+                                        <fieldset className="form-group">
+                                            <label>Message</label>
+                                            <Field className="textarea" type="text" name="message" />
+                                        </fieldset>
+                                        <div className="btn-center">
+                                        <button className="btn" type="submit">Submit</button>
+                                        </div>
+                                    </Form>
+                                )
+                            }
+                        </Formik>
+                    </div>
                 </div>
-            </div>
         )
     }
 }
