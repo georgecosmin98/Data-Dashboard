8a7867ba8f5f7e4c1a8bcfce7c12572dc749ac00
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-31 16:00:02 +0200

    Fix message contact form, now we can encode special characters

--- frontend/datadashboard-app/src/components/api/contact/ContactService.js
+++ frontend/datadashboard-app/src/components/api/contact/ContactService.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
+import { Base64 } from 'js-base64';
 
 class ContactService{
 
     sendMail(from,subject,text){
         console.log(from);
         console.log(subject);
-        console.log(atob(text))
-        return axios.put(`http://localhost:8080/contact/sendMail/${from}/${subject}/${btoa(text)}`)
+        console.log(Base64.encode(text))
+        return axios.put(`http://localhost:8080/contact/sendMail/${from}/${subject}/${Base64.encode(text)}`)
     }
 }
 
