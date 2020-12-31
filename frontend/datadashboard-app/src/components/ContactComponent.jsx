20d1d7b2f6b5324a3e65d12094494a82032dda36
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-31 15:32:07 +0200

    Add validator for empty form fields at contact page

--- frontend/datadashboard-app/src/components/ContactComponent.jsx
+++ frontend/datadashboard-app/src/components/ContactComponent.jsx
@@ -12,77 +12,75 @@ class ContactComponent extends Component {
             email: '',
             message: ''
         }
-        this.onSubmit= this.onSubmit.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+        this.validate = this.validate.bind(this)
     }
-    
-    onSubmit(props){
-        ContactService.sendMail(props.email,props.subject,props.message)
-        .then(()=> this.props.history.push('/contact'))
+
+    onSubmit(values) {
+        ContactService.sendMail(values.email, values.subject, values.message)
+            .then(() => this.props.history.push('/contact'))
+    }
+
+    validate(values) {
+        let errors = {}
+
+        if (!values.email) {
+            errors.email = "Enter a Email address!"
+        }
+        if (!values.subject) {
+            errors.subject = "Enter a subject"
+        }
+        if(!values.message){
+            errors.message = "Enter a message"
+        }
+        return errors
     }
 
     render() {
-        let {subject, email, message } = this.state
+        let { subject, email, message } = this.state
         return (
-            // <div className="contact">
-            //     <div className="form">
-            //         <h1 class="l-heading"><span class="text-primary">Contact </span>US</h1>
-            //         <p>Your questions, comments and feedbacks are important for us!</p>
-            //         <form action="process.php">
-            //             <div class="form-group">
-            //                 <label for="name">Name</label>
-            //                 <input type="text" name="name" id="name" />
-            //             </div>
-            //             <div class="form-group">
-            //                 <label for="email">Email</label>
-            //                 <input type="text" name="email" id="email" />
-            //             </div>
-            //             <div class="form-group">
-            //                 <label for="Message">Message</label>
-            //                 <textarea name="message" id="Message"></textarea>
-            //             </div>
-            //             <div className="btn-center">
-            //                 <button type="submit" className="btn " onSubmit={this.onSubmit}>Submit</button>
-            //             </div>
-            //         </form>
-            //     </div>
-            // </div>
-                <div className="contact">
-                    <div className="form">
-                        <h1 class="l-heading"><span class="text-primary">Contact </span>US</h1>
-                        <p>Your questions, comments and feedbacks are important for us!</p>
-                        <Formik
-                            initialValues={{ subject, email,message }}
-                            onSubmit={this.onSubmit}
-                            enableReinitialize={true}
-                        >
-                            {
-                                (props) => (
-                                    <Form>
-                                        <ErrorMessage name="description" component="div"
-                                            className="alert alert-warning" />
-                                        <ErrorMessage name="targetDate" component="div"
-                                            className="alert alert-warning" />
-                                        <fieldset className="form-group">
-                                            <label>Subject</label>
-                                            <Field className="input" type="text" name="subject" />
-                                        </fieldset>
-                                        <fieldset className="form-group">
-                                            <label>Email</label>
-                                            <Field className="input" type="text" name="email" />
-                                        </fieldset>
-                                        <fieldset className="form-group">
-                                            <label>Message</label>
-                                            <Field className="textarea" type="text" name="message" />
-                                        </fieldset>
-                                        <div className="btn-center">
+            <div className="contact">
+                <div className="form">
+                    <h1 class="l-heading"><span class="text-primary">Contact </span>US</h1>
+                    <p>Your questions, comments and feedbacks are important for us!</p>
+                    <Formik
+                        initialValues={{ subject, email, message }}
+                        onSubmit={this.onSubmit}
+                        validateOnChange={false}
+                        validateOnBlur={false}
+                        validate={this.validate}
+                        enableReinitialize={true}
+                    >
+                        {
+                            (props) => (
+                                <Form>
+                                    <ErrorMessage name="subject" component="div"
+                                        className="alert alert-warning" />
+                                    <ErrorMessage name="email" component="div"
+                                        className="alert alert-warning" />
+                                    <ErrorMessage name="message" component="div"
+                                        className="alert alert-warning" />
+                                    <fieldset className="form-group">
+                                        <label>Subject</label>
+                                        <Field className="input" type="text" name="subject" />
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Email</label>
+                                        <Field className="input" type="text" name="email" />
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Message</label>
+                                        <Field className="textarea" component="textarea" type="text" name="message" />
+                                    </fieldset>
+                                    <div className="btn-center">
                                         <button className="btn" type="submit">Submit</button>
-                                        </div>
-                                    </Form>
-                                )
-                            }
-                        </Formik>
-                    </div>
+                                    </div>
+                                </Form>
+                            )
+                        }
+                    </Formik>
                 </div>
+            </div>
         )
     }
 }
