6656ec6982fc1054def8e1ad16b00b93b408171a
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-31 15:38:55 +0200

    Fix message without newline from contact page/ encode message

--- frontend/datadashboard-app/src/components/api/contact/ContactService.js
+++ frontend/datadashboard-app/src/components/api/contact/ContactService.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 class ContactService{
 
     sendMail(from,subject,text){
-        return axios.put(`http://localhost:8080/contact/sendMail/${from}/${subject}/${text}`)
+        console.log(from);
+        console.log(subject);
+        console.log(atob(text))
+        return axios.put(`http://localhost:8080/contact/sendMail/${from}/${subject}/${btoa(text)}`)
     }
 }
 
