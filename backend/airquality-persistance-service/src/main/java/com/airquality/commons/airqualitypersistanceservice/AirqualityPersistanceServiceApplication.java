1c1ab87ce5fa115647ae708a0dc84a286f65d114
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-01-05 14:25:32 +0200

    Configure backend to allow communication with frontend (CORS and SpringBootServletInitializer)

--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/AirqualityPersistanceServiceApplication.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/AirqualityPersistanceServiceApplication.java
@@ -2,10 +2,17 @@ package com.airquality.commons.airqualitypersistanceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AirqualityPersistanceServiceApplication {
+public class AirqualityPersistanceServiceApplication extends SpringBootServletInitializer {
 
+	//
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(AirqualityPersistanceServiceApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AirqualityPersistanceServiceApplication.class, args);
 	}
--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/controller/ContactController.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/controller/ContactController.java
@@ -2,17 +2,24 @@ package com.airquality.commons.airqualitypersistanceservice.controller;
 
 import com.airquality.commons.airqualitypersistanceservice.service.EmailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contact")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin("*")
 public class ContactController {
 
     @Autowired
     EmailServiceImpl emailService;
 
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
     @PutMapping("/sendMail/{from}/{subject}/{text}")
+    @ResponseStatus(HttpStatus.OK)
     public void sendMail(@PathVariable String from,@PathVariable String subject,@PathVariable String text) throws Exception {
         emailService.sendMail(from, subject, text);
     }
