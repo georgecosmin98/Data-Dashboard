d77d3725819f0cba3c732031d488091ed114d175
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-01-08 11:35:46 +0200

    Add functionality for send confirmation mail after send contact

--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/EmailServiceImpl.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package com.airquality.commons.airqualitypersistanceservice.service;
 import com.airquality.commons.airqualitypersistanceservice.service.api.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,31 @@ public class EmailServiceImpl implements EmailService {
         simpleMailMessage.setSubject(subject);
         simpleMailMessage.setText(new String(Base64.getDecoder().decode(message)));
 
-        javaMailSender.send(simpleMailMessage);
+        try {
+            javaMailSender.send(simpleMailMessage);
+        }catch (MailSendException ex){
+            System.out.println("Failed to send mail");
+        }
+        sendConfirmationMail(from);
+        }
+
+    @Override
+    public void sendConfirmationMail(String to) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        //It's not mandatory to provide the from address, but many SMTP
+        //server would reject such messages
+        simpleMailMessage.setFrom(mailTo);
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject("Harta Poluare Brasov Confirmation Mail");
+        simpleMailMessage.setText("Hello " + to + "\n We received succesfully your message"
+                                    + "\n\n Thank you," +"\n Harta Poluare Brasov Team");
+
+        try {
+            javaMailSender.send(simpleMailMessage);
+        }catch (MailSendException ex){
+            System.out.println("Failed to send confirmation mail");
+        }
     }
+
+
 }
--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/api/EmailService.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/api/EmailService.java
@@ -3,4 +3,5 @@ package com.airquality.commons.airqualitypersistanceservice.service.api;
 public interface EmailService {
 
     void sendMail(String from, String subject, String message);
+    void sendConfirmationMail(String to);
 }
