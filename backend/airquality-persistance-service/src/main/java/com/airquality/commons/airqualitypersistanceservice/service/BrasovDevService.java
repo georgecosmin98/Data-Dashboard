e7492d32c8b7cccd388d2ea512afdb6b4784c1dc
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-01-15 17:49:35 +0200

    Make service class for BrasovDev index

--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/BrasovDevService.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/BrasovDevService.java
@@ -1,9 +1,23 @@
 package com.airquality.commons.airqualitypersistanceservice.service;
 
+import com.airquality.commons.airqualitypersistanceservice.model.BrasovDevDto;
+import com.airquality.commons.airqualitypersistanceservice.repository.BrasovDevRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BrasovDevService {
 
+    @Autowired
+    BrasovDevRepository brasovDevRepository;
 
+    public List<BrasovDevDto> findBySensor(String sensorName) {
+        return brasovDevRepository.findBySensor(sensorName);
+    }
+
+    public List<BrasovDevDto> findByTimestampIsBetween(int firstDate, int lastDate) {
+        return brasovDevRepository.findByTimestampIsBetween(firstDate, lastDate);
+    }
 }
