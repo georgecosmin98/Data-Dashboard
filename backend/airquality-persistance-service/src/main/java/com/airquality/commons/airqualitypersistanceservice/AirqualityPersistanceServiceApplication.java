d2176e4ab057e9eecbd0b8b717e1138f27b7c0af
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-01-15 17:45:59 +0200

    Implement Log4J2

--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/AirqualityPersistanceServiceApplication.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/AirqualityPersistanceServiceApplication.java
@@ -1,14 +1,16 @@
 package com.airquality.commons.airqualitypersistanceservice;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@Log4j2
 public class AirqualityPersistanceServiceApplication extends SpringBootServletInitializer {
 
-	//
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
 		return builder.sources(AirqualityPersistanceServiceApplication.class);
