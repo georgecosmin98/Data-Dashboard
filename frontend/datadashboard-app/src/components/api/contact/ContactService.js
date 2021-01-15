2ed65be9ab545fd4d33de7e41948445f3cee913b
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-01-15 21:17:55 +0200

    Update ContactService.js

--- frontend/datadashboard-app/src/components/api/contact/ContactService.js
+++ frontend/datadashboard-app/src/components/api/contact/ContactService.js
@@ -7,12 +7,13 @@ class ContactService{
         console.log(from);
         console.log(subject);
         console.log(Base64.encode(text))
-        //return axios.put(`http://185.146.87.75:8080/airquality/contact/sendMail/${from}/${subject}/${Base64.encode(text)}`)
-        return axios.put(`http://localhost:8080/contact/sendMail/${from}/${subject}/${Base64.encode(text)}`)
+        return axios.put(`http://185.146.87.75:8080/airquality/contact/sendMail/${from}/${subject}/${Base64.encode(text)}`)
+       // return axios.put(`http://localhost:8080/contact/sendMail/${from}/${subject}/${Base64.encode(text)}`)
     }
 
     verifyEmail(from){
-        return axios.put(`http://localhost:8080/contact/emailVerification/${from}`)
+        return axios.put(`http://185.146.87.75:8080/airquality/contact/emailVerification/${from}`)
+        //return axios.put(`http://localhost:8080/contact/emailVerification/${from}`)
     }
 }
 
