33aa32fdb9f1405c273ec75e963b677243c629c9
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-02-24 16:19:38 +0200

    Create UserService interface and UserService class

---
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.airquality.commons.airqualitypersistanceservice.service;
+
+import com.airquality.commons.airqualitypersistanceservice.model.UserDto;
+import com.airquality.commons.airqualitypersistanceservice.repository.UserRepository;
+import com.airquality.commons.airqualitypersistanceservice.service.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto){
+        return userRepository.save(userDto);
+    }
+}
---
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/api/UserService.java
@@ -0,0 +1,8 @@
+package com.airquality.commons.airqualitypersistanceservice.service.api;
+
+import com.airquality.commons.airqualitypersistanceservice.model.UserDto;
+
+public interface UserService {
+
+    public UserDto createUser(UserDto userDto);
+}
