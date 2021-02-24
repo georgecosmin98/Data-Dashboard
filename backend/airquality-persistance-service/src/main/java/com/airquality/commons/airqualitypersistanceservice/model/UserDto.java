3f837ec31447a1aecc1e38f8895c1cd1a87e8229
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-02-24 16:13:24 +0200

    Create model class UserDto with users properties
    This model class is used for user authentication index

---
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/model/UserDto.java
@@ -0,0 +1,34 @@
+package com.airquality.commons.airqualitypersistanceservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+
+@Data
+//Data is a shortcut for ToString EqualsAndHasCode, Getter, Setter
+//and RequiredArgsConstructor annotation!
+@Document(indexName = "test-user")
+@AllArgsConstructor
+public class UserDto {
+
+    @Id
+    @ReadOnlyProperty
+    private String id;
+
+    @Field(name = "name")
+    private String name;
+
+    @Field(name = "email")
+    private String email;
+
+    @Field(name = "imageUrl")
+    private String imageUrl;
+
+    @Field(name = "password")
+    private String password;
+
+}
