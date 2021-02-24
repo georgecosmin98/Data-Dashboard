ca567dcda20f8274a2d866d82308a3135dd7db08
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-02-24 21:25:25 +0200

    Add functionality for add local user into elasticsearch

--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/model/UserDto.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/model/UserDto.java
@@ -3,16 +3,19 @@ package com.airquality.commons.airqualitypersistanceservice.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Data
 //Data is a shortcut for ToString EqualsAndHasCode, Getter, Setter
 //and RequiredArgsConstructor annotation!
 @Document(indexName = "test-user")
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
     @Id
--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/repository/UserRepository.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.airquality.commons.airqualitypersistanceservice.repository;
 import com.airquality.commons.airqualitypersistanceservice.model.UserDto;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends ElasticsearchRepository<UserDto, String> {
 
-    Optional<UserDto> findByEmail(String email);
 
-    boolean existsByEmail(String email);
-}
+    Optional<UserDto> findByEmailEquals(String email);
+    List<UserDto> findByEmail(String email);
+ }
