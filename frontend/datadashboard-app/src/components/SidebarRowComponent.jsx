d3a32d590d45dd05095eb535f8f24858653b0c82
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-03-25 09:20:39 +0200

    Implement AuthenticatedRoute for private resource

--- frontend/datadashboard-app/src/components/SidebarRowComponent.jsx
+++ frontend/datadashboard-app/src/components/SidebarRowComponent.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 function SidebarRowComponent(props) {
 
     return (
+        //Class name of selected sidebarRow is: sidebarRow selected
+        //Class name of unselected sidebarRos is: sidebarRow
         <div className={`sidebarRow ${props.selected && "selected"}`}>
             <props.icon className={'sidebarRow-icon'} />
             <h2 className="sidebarRow-title">{props.title}</h2>
