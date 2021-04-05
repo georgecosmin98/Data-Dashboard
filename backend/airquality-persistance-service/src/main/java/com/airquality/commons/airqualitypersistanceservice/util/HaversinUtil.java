834150fa95f4b4b3445f094cec6145bed3fe2606
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-04-05 19:59:32 +0300

    Implement haversine formula to calculate distance between two geographic coordinates

---
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/util/HaversinUtil.java
@@ -0,0 +1,20 @@
+package com.airquality.commons.airqualitypersistanceservice.util;
+
+//Haversin formula is used to calculate distance between two
+//geographic coordinates.
+//a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+//c = 2 ⋅ atan2(√a, √(1−a))
+//d = R ⋅ c
+public class HaversinUtil {
+
+    public static double distanceCalculator(double toLatitude, double toLongitude, double fromLatitude, double fromLongitude) {
+        final int earthRadius = 6371;
+        Double latDistance = Math.toRadians(fromLatitude - toLatitude);
+        Double lonDistance = Math.toRadians(fromLongitude - toLongitude);
+        Double a = Math.pow(Math.sin(latDistance / 2), 2) +
+                Math.cos(Math.toRadians(toLatitude)) * Math.cos(Math.toRadians(fromLatitude)) *
+                        Math.pow(Math.sin(lonDistance / 2), 2);
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return earthRadius * c;
+    }
+}
