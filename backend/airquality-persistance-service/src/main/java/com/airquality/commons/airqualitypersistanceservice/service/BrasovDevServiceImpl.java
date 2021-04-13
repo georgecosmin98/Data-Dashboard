30251a21474493ab3db7dd025234241b30d3aabc
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-04-13 22:02:27 +0300

    Implement average aggregation for return average value of pollutants

--- backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/BrasovDevServiceImpl.java
+++ backend/airquality-persistance-service/src/main/java/com/airquality/commons/airqualitypersistanceservice/service/BrasovDevServiceImpl.java
@@ -11,12 +11,15 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.aggregations.Aggregation;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.Aggregations;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.Avg;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -54,30 +57,53 @@ public class BrasovDevServiceImpl implements BrasovDevService {
         List<UserLocationDto> dataFromElasticsearch = userLocationRepository.findUserLocationDtoByTimestampAfterOrderByTimestampAsc(data.getTime());
         Map<Double, Double> sensorData = findUniqueLatitudeAndLongitudeValue();
         List<BrasovDevDto> pollutionDataBasedOnLocation = new ArrayList<BrasovDevDto>();
+        List<UserLocationDto> minMax = userLocationRepository.findUserLocationDtoByTimestampAfterOrderByLatitudeAsc(data.getTime());
+        double minLat = minMax.get(0).getLatitude();
+        double maxLat = minMax.get(minMax.size() - 1).getLatitude();
+        minMax = userLocationRepository.findUserLocationDtoByTimestampAfterOrderByLongitudeAsc(data.getTime());
+        double minLong = minMax.get(0).getLongitude();
+        double maxLong = minMax.get(minMax.size() - 1).getLongitude();
 
-        //Iterate userLocationDto list and find min distance between our coordinates and near sensors
-        for (UserLocationDto userLocationDto : dataFromElasticsearch) {
-            //Key represent Latitude and Value represent Longitude
-            double minDistance = Double.MAX_VALUE;
+        List<BrasovDevDto> pollutionData = brasovDevRepository.findAllBySensorAndLocationLatBetweenAndLocationLongBetweenAndTimestampAfterOrderByTimestampAsc(
+                "pm10", minLat - 0.015, maxLat + 0.015,
+                minLong - 0.015, maxLong + 0.015, data.getTime());
 
-            for (Map.Entry<Double, Double> entry : sensorData.entrySet()) {
-                double distance = HaversinUtil.distanceCalculator(userLocationDto.getLatitude(), userLocationDto.getLongitude(),
-                        entry.getKey(), entry.getValue());
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    nearestLatitude = entry.getKey();
-                    nearestLongitude = entry.getValue();
-                }
-            }
+//        for(UserLocationDto user : dataFromElasticsearch) {
+//            for()
+//        }
+//        }
 
-            List<BrasovDevDto> result = brasovDevRepository.findByTimestampBetweenAndSensorAndLocationLatAndLocationLong(
-                    userLocationDto.getTimestamp().getTime(), userLocationDto.getTimestamp().getTime() + (3600 * 1000), "pm10", nearestLatitude, nearestLongitude);
-            if (!result.isEmpty())
-                pollutionDataBasedOnLocation.add(result.get(0));
-            System.out.println(nearestLatitude + "   =>    " + nearestLongitude + " distanta minima " + minDistance
-                    + "    coordonatele mele: " + userLocationDto.getLatitude() + "=>" + userLocationDto.getLongitude());
-        }
     }
+
+//    public void pollutionDataBasedOnLocation(Date data) throws IOException {
+//        double nearestLatitude = 0;
+//        double nearestLongitude = 0;
+//        List<UserLocationDto> dataFromElasticsearch = userLocationRepository.findUserLocationDtoByTimestampAfterOrderByTimestampAsc(data.getTime());
+//        Map<Double, Double> sensorData = findUniqueLatitudeAndLongitudeValue();
+//        List<BrasovDevDto> pollutionDataBasedOnLocation = new ArrayList<BrasovDevDto>();
+//
+//        //Iterate userLocationDto list and find min distance between our coordinates and near sensors
+//        for (UserLocationDto userLocationDto : dataFromElasticsearch) {
+//            //Key represent Latitude and Value represent Longitude
+//            double minDistance = Double.MAX_VALUE;
+//
+//            for (Map.Entry<Double, Double> entry : sensorData.entrySet()) {
+//                double distance = HaversinUtil.distanceCalculator(userLocationDto.getLatitude(), userLocationDto.getLongitude(),
+//                        entry.getKey(), entry.getValue());
+//                if (distance < minDistance) {
+//                    minDistance = distance;
+//                    nearestLatitude = entry.getKey();
+//                    nearestLongitude = entry.getValue();
+//                }
+//            }
+//            List<BrasovDevDto> result = brasovDevRepository.findByTimestampBetweenAndSensorAndLocationLatAndLocationLong(
+//                    userLocationDto.getTimestamp().getTime(), userLocationDto.getTimestamp().getTime() + (3600 * 1000), "pm25", nearestLatitude, nearestLongitude);
+//            if (!result.isEmpty())
+//                pollutionDataBasedOnLocation.add(result.get(0));
+//            System.out.println(nearestLatitude + "   =>    " + nearestLongitude + " distanta minima " + minDistance
+//                    + "    coordonatele mele: " + userLocationDto.getLatitude() + "=>" + userLocationDto.getLongitude());
+//        }
+//    }
 
     public List<BrasovDevDto> pollutionDataBasedOnAddressLocation(Date data, String sensor, double latitude, double longitude) throws IOException {
         double nearestLatitude = 0;
@@ -86,21 +112,21 @@ public class BrasovDevServiceImpl implements BrasovDevService {
         List<BrasovDevDto> pollutionDataBasedOnLocation = new ArrayList<BrasovDevDto>();
         //Iterate userLocationDto list and find min distance between our coordinates and near sensors
         double minDistance = Double.MAX_VALUE;
-            for (Map.Entry<Double, Double> entry : sensorData.entrySet()) {
-                double distance = HaversinUtil.distanceCalculator(latitude, longitude,
-                        entry.getKey(), entry.getValue());
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    nearestLatitude = entry.getKey();
-                    nearestLongitude = entry.getValue();
-                }
+        for (Map.Entry<Double, Double> entry : sensorData.entrySet()) {
+            double distance = HaversinUtil.distanceCalculator(latitude, longitude,
+                    entry.getKey(), entry.getValue());
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestLatitude = entry.getKey();
+                nearestLongitude = entry.getValue();
             }
+        }
 
-            List<BrasovDevDto> result = brasovDevRepository.findByTimestampBetweenAndSensorAndLocationLatAndLocationLongOrderByTimestampAsc(
-                    data.getTime(), new Date().getTime(), sensor, nearestLatitude, nearestLongitude);
-            System.out.println(nearestLatitude + "   =>    " + nearestLongitude + " distanta minima " + minDistance
-                    + "    coordonatele mele: " + latitude + "=>" + longitude);
-            return result;
+        List<BrasovDevDto> result = brasovDevRepository.findByTimestampBetweenAndSensorAndLocationLatAndLocationLongOrderByTimestampAsc(
+                data.getTime(), new Date().getTime(), sensor, nearestLatitude, nearestLongitude);
+        System.out.println(nearestLatitude + "   =>    " + nearestLongitude + " distanta minima " + minDistance
+                + "    coordonatele mele: " + latitude + "=>" + longitude);
+        return result;
     }
 
 
@@ -116,7 +142,6 @@ public class BrasovDevServiceImpl implements BrasovDevService {
         // Set terms aggregation and subaggregations
         TermsAggregationBuilder aggregation = AggregationBuilders.terms("LocationLat").field("LocationLat");
         aggregation.subAggregation(AggregationBuilders.terms("LocationLong").field("LocationLong"));
-
         //Set aggregations to search source builder
         searchSourceBuilder.aggregation(aggregation);
 
@@ -149,5 +174,49 @@ public class BrasovDevServiceImpl implements BrasovDevService {
         return sensorData;
     }
 
+    public Double findAverageValue() throws IOException {
+
+        Double average = 0D;
+
+        // we limit it to one index, wildcard patterns are working
+        SearchRequest searchRequest = new SearchRequest("brasov-dev");
+
+        // Use a builder to construct the search query
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        // Match query by field name and value
+        MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder("Sensor", "CO2");
+
+        // Range query after a timestamp
+        RangeQueryBuilder rangeQueryBuilder = new RangeQueryBuilder("TimeStamp").gte("161831459000");
+
+        // Bool query that contain match query and range query
+        BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder().must(matchQueryBuilder).must(rangeQueryBuilder);
+
+        // Set query to searchSourceBuilder object
+        searchSourceBuilder.query(boolQueryBuilder);
+
+        // Create an aggregation for return average value of field with name "Value"
+        AvgAggregationBuilder avgAggregationBuilder = AggregationBuilders.avg("avg").field("Value");
+
+        // Set aggregation to searchSourceBuilder object
+        searchSourceBuilder.aggregation(avgAggregationBuilder);
+
+        // Assign search query to search request
+        searchRequest.source(searchSourceBuilder);
+
+        // Run search on elasticsearch
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        // Verify status of elasticsearch client request
+        RestStatus status = searchResponse.status();
+
+        Aggregations firstAggregations = searchResponse.getAggregations();
+
+        if (status == RestStatus.OK)
+            return ((Avg) firstAggregations.get("avg")).getValue();
+        else
+            return null;
+    }
 
 }
