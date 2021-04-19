de83cd2dfcb057ab7869f52547d72a083079098e
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2021-04-19 21:41:38 +0300

    Make frontend-backend communication for retrieve data for Air Pollutants Box

--- frontend/datadashboard-app/src/api/AirQualityService.js
+++ frontend/datadashboard-app/src/api/AirQualityService.js
@@ -5,5 +5,9 @@ class AirQualityService{
     retrieveHomePollutionValues(date,sensor,latitude,longitude){
         return axios.get(`${process.env.REACT_APP_BASE_URL}/brasovdev/${Moment(date).format('YYYY-MM-DD')}/${sensor}/${latitude}/${longitude}`);
       }
+
+    retrievePollutionValuesForAirqualityDashboard(date,latitude,longitude){
+      return axios.get(`${process.env.REACT_APP_BASE_URL}/brasovdev/findAllAfter/${date}/${latitude}/${longitude}`)
+    }
 }
 export default new AirQualityService()
--- frontend/datadashboard-app/src/api/UserLocationService.js
+++ frontend/datadashboard-app/src/api/UserLocationService.js
@@ -5,9 +5,5 @@ class UserLocationService {
   retriveUserLocationAfter(date) {
     return axios.get(`${process.env.REACT_APP_BASE_URL}/usersLocations/findAllAfter/${date}/${sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)}`);
   }
-
-  retrievePollutionValues(date){
-    return axios.get(`${process.env.REACT_APP_BASE_URL}/brasovdev/findAllAfter/2021-04-04`);
-  }
 }
 export default new UserLocationService()
